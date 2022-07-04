71dc5745cb56986a16616040e49d5089cee0bcb9
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-07-04 16:20:39 -0500

    this is who we are

--- Objetos_Dentro_de_Arrays/middle_age.js
+++ Objetos_Dentro_de_Arrays/middle_age.js
@@ -26,7 +26,7 @@ const bigLibrary = [
     {
         first_part: "Edad Moderna",
         second_part: "Capitalismo urbano",
-        age_date: 1400,
+        age_date: 1500,
         cleanBook: function(){
             console.log(`Limpiando ${this.second_part}`);
         }
@@ -60,12 +60,33 @@ const bigLibrary = [
 let studentVisit = bigLibrary.filter( function(bigLibrary){
     return bigLibrary.first_part == "Edad Ultrapostmoderna"
 });
-    
 console.log(studentVisit);
 
 let teacherVisit = bigLibrary.map( function(bigLibrary) {
     return bigLibrary.age_date
     }
 );
-
 console.log(teacherVisit);
+
+//si el libro existe lo voy a regresar sino no. Genera un nuevo array
+
+var libraryAdviser = bigLibrary.find (function(book) {
+    return book.second_part === "Crisis de la salud"
+}); 
+console.log(libraryAdviser);
+
+// No genera un nueva array.
+bigLibrary.forEach(function(book){
+    console.log(book.second_part)
+});
+
+// otro metodo que es una variacion de verdadero o falso para filtrar
+var centuryAges = bigLibrary.some(function(book){
+    return book.age_date <= 1000;
+})
+
+console.log(centuryAges);
+
+bigLibrary.forEach(function(book){
+    console.log(book.age_date >= 1000);
+});
