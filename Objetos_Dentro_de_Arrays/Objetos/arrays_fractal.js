f4c9bc2b9944d0f9f690151cff4f3efaeabdc392
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-07-04 20:47:57 -0500

    tree leaves as start at the sky

--- Objetos_Dentro_de_Arrays/Objetos/arrays_fractal.js
+++ Objetos_Dentro_de_Arrays/Objetos/arrays_fractal.js
@@ -14,3 +14,25 @@ console.log(fruits.length);
 console.log(fruits[0]);
 console.log(fruits);
 console.log(organize);
+
+//crear una funcion para agregar elementos con push
+const vegetables = [ "Carrot", "Eggplant", "Zucchini", "Tomato"]
+
+function newVegetable(){
+    vegetables.push("Pumpkin", "Onion");
+    console.log(vegetables);
+}
+console.log(newVegetable());
+
+//crear una funcion para agregar elementos con push
+function newBox(){
+    vegetables.shift();
+    console.log(vegetables);
+}
+console.log(newBox());
+
+function box_one(){
+    vegetables.pop();
+    console.log(vegetables);
+}
+console.log(box_one());
--- Objetos_Dentro_de_Arrays/To_do/Loop.js
+++ Objetos_Dentro_de_Arrays/To_do/Loop.js
@@ -1,3 +1,4 @@
+/*
 // mientras adentro de ese loop se cumpla una condicion esa tarea pasa 
 const disciples = ["Jorge", "Adam", "Sweetie", "Nicole", "David"]
 //saludar a cada uno
@@ -17,9 +18,33 @@ Hola Adam
 Sweetie
 Nicole 
 David
- */
+
 
 for (var disciple of disciples) {
     firstClass(disciple);
 }
-    
+
+
+// práctica: escribe un códido en Javascript que le pregunte a los usuarios cuánto es 2 + 2. Si responden bien, mostramos el mensaje, felicitaciones sino volvemos a empezar.
+var start = prompt("Hi please answer how much is 2 + 2");
+*/
+var prompt = require('prompt');
+
+//
+// Start the prompt
+//
+prompt.start();
+
+//
+// Get two properties from the user: username and email
+//
+prompt.get(['2+2'], function (result) {
+    //
+    // Log the results.
+    //
+    if (result = 4) {
+        console.log('Nice!');
+    } else {
+        console.log('Try again');
+    }
+}) 
