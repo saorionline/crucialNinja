a8caee90227a3c7fc1b5fc77e58e97f6ada33ce6
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-06-12 21:02:10 -0500

    Restaurant Pltzi Food

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary': '#353535'
+      },
+      height: {
+        '80': '20rem',
+        '100': '25rem'
+      }
+    },
+  },
+  plugins: [],
+  content: [
+    "./public/index.html",
+  ], 
+}
+
