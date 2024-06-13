d97a1e9251cc3eff99cee4c658e9682959f9bd60
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-06-13 09:37:01 -0500

    First Draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        'primary': '#f3c614',
-        'secondary': '#353535'
-      },
-      height: {
-        '80': '20rem',
-        '100': '25rem'
-      }
-    },
+		  backgroundImage: {
+			  'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+			  'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+			  'yosemite': "url('../img/yosemite.jpg')",
+			  'LA': "url('../img/LA.jpg')",
+			  'seattle': "url('../img/seattle.jpg')",
+			  'new_york': "url('../img/new_york.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'sydney': "url('../img/sydney.jpg')",
+			  'miami': "url('../img/miami.jpg')",
+			  'switzerland': "url('../img/switzerland.jpg')",
+			  'bali': "url('../img/bali.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'chicago': "url('../img/chicago.jpg')",
+			  'europe': "url('../img/europe.jpg')",
+			  'iceland': "url('../img/iceland.jpg')",
+			  
+			},
+			backgroundColor: theme =>({
+				...theme('colors'),
+				'principal': '#CC2D4A',
+				'secondary': '#8FA206',
+				'tertiary': '#61AEC9',
+			}),
+			textColor:{
+				'principal': '#CC2D4A',
+				'secondary': '#8FA206',
+				'tertiary': '#61AEC9',
+			},
+			fontFamily: {
+				Montserrat: ["Montserrat", "sans-serif"],
+			 },
+			
+		},
+	},
+	variants: {
+		width: ["responsive", "hover", "focus"],
+		extend: {
+	 },
   },
-  plugins: [],
   content: [
-    "./public/index.html",
+    "./public/**/*.{png, jpg, css, svg}", 
+    "./src/**/*.css"
   ], 
 }
 
